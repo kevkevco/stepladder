746012c6e9833d2e371fe59666f36581024df10f
kevkevco <92135778+kevkevco@users.noreply.github.com>
2024-01-05 00:18:51 -0500

    Restructure frontend and backend into separate folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css' // TODO
 import './styles/tailwind.css'
-import Link from './Link.jsx';
+import Link from './components/Link.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
