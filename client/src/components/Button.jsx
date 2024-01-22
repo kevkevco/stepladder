8187a7864fed8dd1500f618ad99c19d15bbadaf0
kevkevco <92135778+kevkevco@users.noreply.github.com>
2024-01-22 16:55:19 -0500

    Add button component

---
+++ client/src/components/Button.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+function Button({ colors, text, onClick }) {
+    let styles = colors + `
+        transition-all
+        ease-in-out
+        duration-300
+        py-2
+        px-4
+        rounded-0 // -lg
+        inline-block
+        whitespace-nowrap
+        hover:shadow-lg`;
+    return (<Link to={onClick} className={styles}>{text}</Link>);
+}
+
+export default Button;
