675550968f074bae15f138467aaffa6396929eb5
kevkevco <92135778+kevkevco@users.noreply.github.com>
2024-01-25 19:22:10 -0500

    Improve development ergonomics and add semicolons to Prettier config

--- client/prettier.config.cjs
+++ client/prettier.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import("prettier").Config} */
 module.exports = {
   trailingComma: 'es5',
-  semi: false,
+  semi: true,
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
   htmlWhitespaceSensitivity: 'css',
   bracketSpacing: true,
   plugins: ['prettier-plugin-tailwindcss'],
-};
+}
